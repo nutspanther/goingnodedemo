ef6315ce65540bb28b81ca5486096ed91bae0bf0
Trey Hecht <thecht@netroadshow.com>
2013-10-21 13:23:16 -0700

    creating the chat branch

--- index.js
+++ index.js
@@ -8,9 +8,21 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+var chat = io
+    .of('/chat')
+    .on('connection', function (socket) {
+        socket.emit('a message', {
+            that: 'only'
+            , '/chat': 'will get'
+        });
+        chat.emit('a message', {
+            everyone: 'in'
+            , '/chat': 'will get'
+        });
+    });
+
+var news = io
+    .of('/news')
+    .on('connection', function (socket) {
+        socket.emit('item', { news: 'item' });
+    });
