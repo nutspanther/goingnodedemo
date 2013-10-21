e0c72bdd282b0d4375a5dfb9f2c6a41487b002a1
Beau Watson <bwatson@fingerpaintmarketing.com>
2013-10-21 14:42:10 -0400

    Inital commit

---
+++ README.md
@@ -0,0 +1,10 @@
+goingnode
+=========
+
+Examples for the Going Node training @ HTMLDevConf2013
+
+### Going Node Companion
+http://www.scribd.com/doc/177682754/Going-Node-Companion
+
+### Going Node Presentation
+https://docs.google.com/a/busyrich.com/presentation/d/1HtaxwynVqw4YZFZKz8K7so4tb1M7qVNEenPf8boEtNk/pub?start=false&loop=false&delayms=3000#slide=id.p
---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    db = mongoose.connection,
+    latest = {};
+
+//Listen to some database connection events
+db.on('error', console.error);
+db.once('open', function() {
+  console.log('Database Connected.');
+});
+
+//Connect to the database
+mongoose.connect('mongodb://THEteam:password12345@mongo.onmodulus.net:27017/paqe9huJ');
+
+//Some Server configuration
+app.use(express.bodyParser());
+app.set('view engine', 'ejs');
+
+//Create the movie schema
+var movieSchema = new mongoose.Schema({
+  title: { type: String },
+  mpaa: String,
+  release: Number
+});
+
+//Movie database object
+var Movie = mongoose.model('Movie', movieSchema);
+
+//Initialize the cache with a movie
+Movie.findOne({title:'Tremors'}, function(err, movie) {
+  if(err) {
+    return console.log('Error getting latest %j', err);
+  }
+
+  latest = movie;
+});
+
+//Get latest movie
+app.get('/', function(req, res) {
+  res.render('index', latest);
+});
+
+//Save a movie, set it to the latest
+app.post('/', function(req, res) {
+  new Movie(req.body).save(function(err, movie) {
+    if(err) {
+      return res.send(err.message);
+    }
+
+    latest = movie;
+    res.send(movie.title + ' saved.');
+  });
+});
+
+//Start the app
+app.listen(2013);
