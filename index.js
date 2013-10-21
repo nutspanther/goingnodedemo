4247f71afd9911d9ff51637487d6e07189a43316
Beau Watson <bwatson@fingerpaintmarketing.com>
2013-10-21 16:52:15 -0400

    Mongoose integration

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require('express'),
     app = express(),
-    server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+    server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+  	mongoose = require('mongoose'),
+    db = mongoose.connection;
 
 server.listen(2014);
+
+//Listen to some database connection events
+db.on('error', console.error);
+db.once('open', function() {
+  console.log('Database Connected.');
+});
+
+//Connect to the database
+mongoose.connect('mongodb://THEteam:password12345@mongo.onmodulus.net:27017/paqe9huJ');
 
 app.use( express.static( __dirname + '/public' ) );
 
