2677d46b32772cb05a918547aa57cc42555b1a3a
Eric Hall <eric@oceanbeach.com>
2013-10-21 14:50:07 -0700

    First pass at location schema with dummy info.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-	io = require('socket.io').listen(server),
+	  io = require('socket.io').listen(server),
   	mongoose = require('mongoose'),
-    db = mongoose.connection;
-
+    db = mongoose.connection,
+    schema = mongoose.Schema;
 server.listen(2014);
 
 //Listen to some database connection events
@@ -15,6 +15,41 @@ db.once('open', function() {
 
 //Connect to the database
 mongoose.connect('mongodb://THEteam:password12345@mongo.onmodulus.net:27017/paqe9huJ');
+
+
+//Create the movie schema
+var locationSchema = new mongoose.Schema({
+  name: String ,
+  north : schema.Types.ObjectId,
+  south : schema.Types.ObjectId,
+  east  : schema.Types.ObjectId,
+  west  : schema.Types.ObjectId,
+  up    : schema.Types.ObjectId,
+  down  : schema.Types.ObjectId,
+});
+
+var Location = mongoose.model( "Location", locationSchema );
+
+var Lobby = new Location( { name: "lobby" } );
+var EastRoom = new Location( { name: "east room" } );
+var WestRoom = new Location( { name: "west room" } );
+var NorthRoom = new Location( { name: "north room" } );
+var MainRoom = new Location( { name: "main room",
+                               north: NorthRoom,
+                               east:  EastRoom,
+                               west:  WestRoom,
+                               south: Lobby
+} );
+
+Lobby.north = MainRoom;
+EastRoom.west = MainRoom;
+WestRoom.east = MainRoom;
+NorthRoom.south = MainRoom;
+MainRoom.save();
+Lobby.save();
+EastRoom.save();
+NorthRoom.save();
+WestRoom.save();
 
 app.use( express.static( __dirname + '/public' ) );
 
